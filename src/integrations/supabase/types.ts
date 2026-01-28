651d998ade88240723a0ef25584ba5d78d53b039
softgenai <admin@softgen.ai>
2026-01-28 03:51:32 +0000

    fix(brand-page): Correctly display funnels and total leads

--- src/integrations/supabase/types.ts
+++ src/integrations/supabase/types.ts
@@ -12,4 +12,4 @@ export type Enums<T extends keyof Database['public']['Enums']> = Database['publi
 export type Functions<T extends keyof Database['public']['Functions']> = Database['public']['Functions'][T];
 
 // Project reference: dqlzthetcaktudilsyxu
-// Last updated: 2026-01-28T03:48:09.703834
+// Last updated: 2026-01-28T03:50:19.836240
